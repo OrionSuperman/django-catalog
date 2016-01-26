30537f046c7ef36639326c9b096bc9a1c2aed33f
Orion Supergan <orionsuperman@gmail.com>
2016-01-25 18:16:17 -0800

    django entries into database and pulling info

--- apps/review/urls.py
+++ apps/review/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from . import views
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
-  url(r'^create/$', views.create),
-  url(r'^product/(?P<idx>\d+)/$', views.product)
+  url(r'^create/$', views.create, name='cat-create'),
+  url(r'^product/(?P<idx>\d+)/$', views.product),
+  url(r'^update/$', views.update, name='cat-update'),
+  url(r'^delete/(?P<idx>\d+)/$', views.delete),
 )
--- apps/review/views.py
+++ apps/review/views.py
@@ -1,10 +1,40 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect # inserted this line
+from .models import Manufacturer, Product
 def index(request):
-  return render(request, 'quiz/index.html') # updated this line 
+	context = Product.objects.all()
+
+	mfg = Manufacturer.objects.all()
+	print mfg
+	for company in mfg:
+		print company.name
+	return render(request, 'review/index.html',{'context' : context, 'mfg' : mfg}) # updated this line 
 
 def create(request):
-	pass
+	mfg = Manufacturer.objects.get(id=request.POST['manufacturer'])
+	review = Product(name = request.POST['name'], price = request.POST['price'], description = request.POST['description'], manufacturer = mfg)
+	review.save()
 
-def product(request):
-	pass
+	return redirect('/')
+
+def product(request, idx):
+	item = Product.objects.get(id=idx)
+	mfg = Manufacturer.objects.all()
+	return render(request, 'review/item.html', {'item':item, 'mfg':mfg})
+
+def update(request):
+	item = Product.objects.get(id=request.POST['id'])
+	item.name = request.POST['name']
+	item.manufacturer = Manufacturer.objects.get(id=request.POST['manufacturer'])
+	item.price = request.POST['price']
+	item.description = request.POST['description']
+	item.save()
+
+	return redirect('/product/'+request.POST['id'])
+
+
+def delete(request, idx):
+	item = Product.objects.get(id=idx)
+	item.delete()
+
+	return redirect('/')
